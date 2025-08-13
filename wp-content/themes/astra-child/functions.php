b81fa5b64ee5481ff672b1d03a78c6f650d7720d
jubayer1431 <mdhridoy625@gmail.com>
2025-08-13 10:24:09 +0600

    side sticky navi

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -5,3 +5,10 @@ function astra_child_enqueue_styles()
     wp_enqueue_style('astra-parent-style', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'astra_child_enqueue_styles');
+
+
+add_action('wp_footer', function () {
+    if (! is_page(42)) {
+        echo do_shortcode('[elementor-template id="532"]');
+    }
+});
