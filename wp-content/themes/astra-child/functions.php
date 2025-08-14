fc80d6a25b6046b09db4bb170c58f440d3cb3303
Md Muhimenul Tareque <mmtareque@yahoo.com>
2025-08-14 23:00:33 +0600

    add post filter and dynamic sticky post

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -16,74 +16,136 @@ add_action('wp_footer', function () {
 
 
 // Register Shortcode
-function get_categories_with_counts_shortcode()
-{
+
+
+function get_categories_with_counts_shortcode() {
     $categories = get_categories([
-        'hide_empty' => false, // show categories even if count = 0
+        'hide_empty' => false,
     ]);
 
-    ob_start(); // Start capturing HTML
+    ob_start();
 
     echo '<div class="category-cards-wrapper">';
     foreach ($categories as $category) {
+        // Use your blog page URL + query param
+        $category_link = add_query_arg('filter_cat', $category->term_id, site_url('/blogs/'));
 ?>
-
-
-
-
-
-
-        <div class="elementor-widget-wrap elementor-flex-align-center elementor-element-populated elementor-inline-flex">
+        <a href="<?php echo esc_url($category_link); ?>" class="elementor-widget-wrap elementor-flex-align-center elementor-element-populated elementor-inline-flex category-link" style="text-decoration: none;">
             <div class="elementor-heading-title elementor-size-default category-name">
                 <?php echo esc_html($category->name); ?>
             </div>
             <div class="elementor-badge elementor-badge-number category-badge">
                 <?php echo esc_html($category->count); ?>
             </div>
-        </div>
-
-
-
+        </a>
 <?php
     }
-
     echo '</div>';
-    return ob_get_clean(); // Return HTML
+    return ob_get_clean();
 }
+
+
+// add_shortcode('categories_with_counts', 'get_categories_with_counts_shortcode');
+
 
 
 add_shortcode('category_cards', 'category_cards_shortcode');
 
 
+function shortcode_latest_sticky_post_data( $atts ) {
+    $atts = shortcode_atts( array(
+        'data' => 'title', // default
+    ), $atts, 'latest_sticky_post' );
 
+    // Get sticky posts
+    $sticky_posts = get_option( 'sticky_posts' );
 
-
-function get_categories_with_counts_shortcode_x()
-{
-    $categories = get_categories(['hide_empty' => false]);
-    ob_start();
-
-    // foreach ($categories as $category) {
-    //     // Pass data to Elementor template via dynamic tags
-    //     echo do_shortcode('[elementor-template id="783"]'); // Replace 1234 with your template ID
-    // }
-
-    foreach ($categories as $category) {
-        set_query_var('cat_name', $category->name);
-        set_query_var('cat_count', $category->count);
-
-        echo do_shortcode('[elementor-template id="783"]');
+    if ( empty( $sticky_posts ) ) {
+        return '';
     }
 
+    // Query the latest sticky post
+    $args = array(
+        'post__in'            => $sticky_posts,
+        'posts_per_page'      => 1,
+        'ignore_sticky_posts' => 1,
+        'orderby'             => 'date',
+        'order'               => 'DESC'
+    );
 
-    //     foreach ($categories as $category) {
-    //     echo do_shortcode(
-    //         '[elementor-template id="783" cat_name="' . esc_attr($category->name) . '" cat_count="' . esc_attr($category->count) . '"]'
-    //     );
-    // }
+    $query = new WP_Query( $args );
 
-    return ob_get_clean();
+    if ( ! $query->have_posts() ) {
+        return '';
+    }
+
+    $sticky_post = $query->posts[0]; // get the post object directly
+
+    // Make sure functions use this post
+    $post_id = $sticky_post->ID;
+
+    
+
+    $result = '';
+    switch ( strtolower( $atts['data'] ) ) {
+        
+        case 'title':
+            $result = get_the_title( $post_id );
+            break;
+
+        case 'url':
+            $result = esc_url( get_permalink( $post_id ) );
+            break;
+
+        case 'image':
+            if ( has_post_thumbnail( $post_id ) ) {
+                $result = esc_url_raw( get_the_post_thumbnail_url( $post_id, 'full' ) );
+            } else {
+                $result = esc_url_raw( get_template_directory_uri() . '/assets/default-placeholder.png' );
+            }
+            break;
+
+        case 'category':
+            $categories = get_the_category( $post_id );
+            if ( ! empty( $categories ) ) {
+                $result = esc_html( $categories[0]->name );
+            }
+            break;
+
+        case 'date':
+            $result = get_the_date( '', $post_id );
+            break;
+
+        case 'author':
+            $result = esc_html( get_the_author_meta( 'display_name', $sticky_post->post_author ) );
+            break;
+
+        case 'tags':
+            $tags = get_the_tags( $post_id );
+            if ( ! empty( $tags ) ) {
+                $tag_spans = array();
+                foreach ( $tags as $tag ) {
+                    $tag_spans[] = '<span class="post-tag">' . esc_html( $tag->name ) . '</span>';
+                }
+                $result = implode( ' ', $tag_spans );
+            }
+            break;
+
+        case 'excerpt':
+            $content = get_post_field( 'post_content', $post_id );
+            $content = wp_strip_all_tags( $content );
+            $result  = esc_html( mb_strimwidth( $content, 0, 200, '...', 'UTF-8' ) );
+            break;
+
+        default:
+            $result = '';
+            break;
+    }
+
+    return $result;
 }
+
+add_shortcode( 'latest_sticky_post', 'shortcode_latest_sticky_post_data' );
 
 add_shortcode('category_counts', 'get_categories_with_counts_shortcode');
 
@@ -125,5 +187,13 @@ add_action('elementor/query/featured_posts', function( $query ) {
         $query->set( 'ignore_sticky_posts', 1 ); // Prevent normal sticky behavior
     } else {
         $query->set( 'post__in', array(0) ); // No sticky posts, return empty
+    }
+});
+
+
+add_action( 'elementor/query/blog_loop', function( $query ) {
+    if ( isset($_GET['filter_cat']) && !empty($_GET['filter_cat']) ) {
+        $cat_id = intval($_GET['filter_cat']);
+        $query->set( 'cat', $cat_id ); // filter by category
     }
 });
